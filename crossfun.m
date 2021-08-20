7c4169abc40d057c69812bb7168c11b84d89a048
RASHMI KRISHNAMOHAN <89169754+rashmikmohan@users.noreply.github.com>
2021-08-19 23:33:09 -0400

    Delete crossfun.m

--- crossfun.m
+++
@@ -1,6 +0,0 @@
-% Cross Validation
-function yfit = crossfun(xtrain,ytrain,xtest,rbf_sigma,boxconstraint)
-svmStruct = svmtrain(xtrain,ytrain,'Kernel_Function','rbf','boxconstraint',boxconstraint);
-yfit = svmclassify(svmStruct,xtest);
-c = cvpartition(200,'kfold',10);
-minfn = @(z)crossval(
